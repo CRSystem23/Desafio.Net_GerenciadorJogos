2004c9c09c5772f3c5e4643e156b688d8b49d375
CRSystem23 <cristiano.ribeiro.cv@gmail.com>
2020-12-22 19:52:36 -0300

    Ajustado controleEmprestimoJogo pra trazer todos os jogos mesmo que não tiver sido emprestado ainda

---
+++ GerenciadorJogos - v04/GerenciadorJogos.AcessoDados/Contexto/GerenciadorJogosDbContext.cs
@@ -0,0 +1,24 @@
+﻿using GerenciadorJogos.AcessoDados.ConfigEntidades;
+using GerenciadorJogos.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorJogos.AcessoDados.Contexto
+{
+    public class GerenciadorJogosDbContext : DbContext
+    {
+        public GerenciadorJogosDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Jogo> Jogo { get; set; }
+        public DbSet<ControleEmprestimoJogo> ControleEmprestimoJogo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PessoaConfiguration());
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+        }
+    }
+}
---
+++ GerenciadorJogos - v04/GerenciadorJogos.Generico/Interface/IRepositorioGenerico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GerenciadorJogos.Generico.Interface
+{
+    public interface IRepositorioGenerico<TDominio, TChave> where TDominio : class
+    {
+        List<TDominio> Selecionar(Expression<Func<TDominio, bool>> where = null);
+        TDominio SelecionarPorId(TChave id);
+        void Inserir(TDominio dominio);
+        void Atualizar(TDominio dominio);
+        void Excluir(TDominio dominio);
+        void ExcluirPorId(TChave id);
+    }
+}
