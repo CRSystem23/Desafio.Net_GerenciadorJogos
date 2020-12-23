01073ff4d04b429043b776a4ddab0ec218587981
CRSystem23 <cristiano.ribeiro.cv@gmail.com>
2020-12-23 02:38:04 -0300

    Finalizando testes

---
+++ GerenciadorJogos - v05/GerenciadorJogos.WebApi/Controllers/PessoaController.cs
@@ -0,0 +1,154 @@
+﻿using GerenciadorJogos.Dominio.Entidades;
+using GerenciadorJogos.Generico.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace GerenciadorJogos.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly IRepositorioGenerico<Pessoa, int> _pessoaRepositorio;
+        private readonly IRepositorioGenerico<Usuario, int> _usuarioRepositorio;
+
+        public PessoaController(IRepositorioGenerico<Pessoa, int> pessoaRepositorio, IRepositorioGenerico<Usuario, int> usuarioRepositorio)
+        {
+            _pessoaRepositorio = pessoaRepositorio;
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<Pessoa> pessoas = _pessoaRepositorio.Selecionar();
+
+                return Ok(pessoas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("not-user")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        public IActionResult GetNotUser()
+        {
+            try
+            {
+                List<Usuario> usuarios = _usuarioRepositorio.Selecionar();
+                List<Pessoa> pessoas = _pessoaRepositorio.Selecionar();
+
+                if(usuarios != null && usuarios.Count > 0 && pessoas != null && pessoas.Count > 0)
+                {
+                    foreach (var usuario in usuarios)
+                    {
+                        pessoas.Remove(usuario.Pessoa);
+                    }
+                }
+
+                return Ok(pessoas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                Pessoa pessoa = _pessoaRepositorio.SelecionarPorId(id);
+
+                if (pessoa == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(pessoa);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        public IActionResult Post([FromBody] Pessoa pessoa)
+        {
+            try
+            {
+                _pessoaRepositorio.Inserir(pessoa);
+
+                return CreatedAtRoute(new { id = pessoa.Id }, pessoa);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        public IActionResult Put(int id, [FromBody] Pessoa pessoa)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+
+                _pessoaRepositorio.Atualizar(pessoa);
+
+                return Ok(pessoa);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+
+                Pessoa pessoa = _pessoaRepositorio.SelecionarPorId(id);
+
+                if (pessoa == null)
+                {
+                    return NotFound();
+                }
+
+                _pessoaRepositorio.ExcluirPorId(id);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ GerenciadorJogos - v05/GerenciadorJogos.WebApi/DTOs/UsuarioDTO.cs
@@ -0,0 +1,51 @@
+﻿using GerenciadorJogos.Generico.Utilitario;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorJogos.WebApi.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        [StringLength(maximumLength: 20, MinimumLength = 5,
+            ErrorMessage = "O login deve conter entre 5 e 20 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Dados imcompletos")]
+        public int PessoaId { get; set; }
+
+        public string PessoaNome { get; set; }
+
+        [Required(ErrorMessage = "A Senha é obrigatória")]
+        [StringLength(maximumLength: 20, MinimumLength = 5,
+            ErrorMessage = "A Senha deve conter entre 5 e 20 caracteres")]
+        public string Senha { get; set; }
+        public bool? IsAdmin { get; set; }
+
+
+        public void Preencher_Propriedades_Para_Insercao()
+        {
+            Senha = this.Retornar_Senha_Hash(Senha);
+            Login = Login.ToLower();
+            PessoaId = PessoaId;
+            IsAdmin = IsAdmin == null ? false : IsAdmin;
+        }
+
+        public void Preencher_Propriedades_Para_Alteracao()
+        {
+            Senha = CriptografiaHash.GerarHash(Senha);
+            Login = Login.ToLower();
+        }
+
+        public string Retornar_Senha_Hash(string senha)
+        {
+            if (string.IsNullOrEmpty(senha))
+            {
+                return senha;
+            }
+
+            return CriptografiaHash.GerarHash(senha);
+        }
+    }
+}
